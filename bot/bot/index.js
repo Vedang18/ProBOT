f4e606b1b40f6fdabda24157226c7d681dd710d4
Abhisheks Simla <abhishek.130490@gmail.com>
2017-09-13 00:46:04 +0530

    Added a basic echo bot

---
+++ bot/bot/index.js
@@ -0,0 +1,87 @@
+var builder = require('botbuilder');
+
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send('echo: ' + session.message.text);
+});
+
+// Enable Conversation Data persistence
+bot.set('persistConversationData', true);
+
+// Set default locale
+bot.set('localizerSettings', {
+    botLocalePath: './bot/locale',
+    defaultLocale: 'en'
+});
+
+
+
+// Trigger secondary dialogs when 'settings' or 'support' is called
+bot.use({
+    botbuilder: function (session, next) {
+        var text = session.message.text;
+        var supportRegex = localizedRegex(session, ['help']);
+
+        if (supportRegex.test(text)) {
+            // interrupt and trigger 'help' dialog
+            //return session.beginDialog('help:/');
+        }
+
+        // continue normal flow
+        next();
+    }
+});
+
+// Send welcome when conversation with bot is started, by initiating the root dialog
+bot.on('conversationUpdate', function (message) {
+    if (message.membersAdded) {
+        message.membersAdded.forEach(function (identity) {
+            if (identity.id === message.address.bot.id) {
+                bot.beginDialog(message.address, '/');
+            }
+        });
+    }
+});
+
+// Cache of localized regex to match selection from main options
+var LocalizedRegexCache = {};
+function localizedRegex(session, localeKeys) {
+    var locale = session.preferredLocale();
+    var cacheKey = locale + ":" + localeKeys.join('|');
+    if (LocalizedRegexCache.hasOwnProperty(cacheKey)) {
+        return LocalizedRegexCache[cacheKey];
+    }
+
+    var localizedStrings = localeKeys.map(function (key) { return session.localizer.gettext(locale, key); });
+    var regex = new RegExp('^(' + localizedStrings.join('|') + ')', 'i');
+    LocalizedRegexCache[cacheKey] = regex;
+    return regex;
+}
+
+// Connector listener wrapper to capture site url
+var connectorListener = connector.listen();
+function listen() {
+    return function (req, res) {
+        connectorListener(req, res);
+    };
+}
+
+// Other wrapper functions
+function beginDialog(address, dialogId, dialogArgs) {
+    bot.beginDialog(address, dialogId, dialogArgs);
+}
+
+function sendMessage(message) {
+    bot.send(message);
+}
+
+module.exports = {
+    listen: listen,
+    beginDialog: beginDialog,
+    sendMessage: sendMessage
+};
