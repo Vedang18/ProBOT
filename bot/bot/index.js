b70cde815c5910e2dc55274fbd1883803d7a32d8
Vedang <vedang.mahajan@prorigosoftware.com>
2017-09-14 18:17:37 +0530

    few more enhancements

--- bot/bot/index.js
+++ bot/bot/index.js
@@ -6,22 +6,110 @@ var connector = new builder.ChatConnector({
 });
 
 // This is a dinner reservation bot that uses a waterfall technique to prompt users for input.
-var bot = new builder.UniversalBot(connector, [
+var bot = new builder.UniversalBot(connector,  function(session){
+    var msg = "Welcome to the reservation bot. Please say `Dinner Reservation` or `Order Dinner`";
+    session.send(msg);
+    //builder.Prompts.choice(session,msg,"Dinner Reservation|Order Dinner", { listStyle: 3 });
+});
+
+// Dialog to ask for a date and time
+bot.dialog('askForDateTime', [
     function (session) {
-        session.send("Welcome to the dinner reservation.");
         builder.Prompts.time(session, "Please provide a reservation date and time (e.g.: June 6th at 5pm)");
     },
     function (session, results) {
-        session.dialogData.reservationDate = builder.EntityRecognizer.resolveTime([results.response]);
+        session.endDialogWithResult(results);
+    }
+]);
+
+// Dialog to ask for number of people in the party
+bot.dialog('askForPartySize', [
+    function (session) {
         builder.Prompts.text(session, "How many people are in your party?");
     },
     function (session, results) {
-        session.dialogData.partySize = results.response;
+        session.endDialogWithResult(results);
+    }]);
+
+    // Dialog to ask for the reservation name.
+bot.dialog('askForReserverName', [
+    function (session) {
         builder.Prompts.text(session, "Who's name will this reservation be under?");
     },
     function (session, results) {
+        session.endDialogWithResult(results);
+    }
+]);
+
+    // Dialog to ask for the Table cloth.
+    bot.dialog('askForTableCloth', [
+        function (session) {
+            builder.Prompts.choice(session, "Table cloth color preferance?", "red|green|blue", { listStyle: 3 });
+        },
+        function (session, results) {
+            session.endDialogWithResult(results);
+        }
+    ]);
+// This dialog help the user order dinner to be delivered to their hotel room.
+var dinnerMenu = {
+    "Potato Salad - $5.99": {
+        Description: "Potato Salad",
+        Price: 5.99
+    },
+    "Tuna Sandwich - $6.89": {
+        Description: "Tuna Sandwich",
+        Price: 6.89
+    },
+    "Clam Chowder - $4.50":{
+        Description: "Clam Chowder",
+        Price: 4.50
+    }
+};
+
+bot.dialog('orderDinner', [
+    function(session){
+        session.send("Lets order some dinner!");
+        builder.Prompts.choice(session, "Dinner menu:", dinnerMenu, { listStyle: 2 });
+    },
+    function (session, results) {
+        if (results.response) {
+            var order = dinnerMenu[results.response.entity];
+            var msg = `You ordered: ${order.Description} for a total of $${order.Price}.`;
+            session.dialogData.order = order;
+            session.send(msg);
+            builder.Prompts.text(session, "What is your room number?");
+        } 
+    },
+    function(session, results){
+        if(results.response){
+            session.dialogData.room = results.response;
+            var msg = `Thank you. Your order will be delivered to room #${session.dialogData.room}`;
+            session.endDialog(msg);
+        }
+    }
+])
+.triggerAction({
+    matches: /^order dinner$/i,
+    confirmPrompt: "This will cancel your order. Are you sure?"
+});
+
+// This dialog helps the user make a dinner reservation.
+bot.dialog('dinnerReservation', [
+    function (session) {
+        session.send("Welcome to the dinner reservation.");
+        session.beginDialog('askForDateTime');
+    },
+    function (session, results) {
+        session.dialogData.reservationDate = builder.EntityRecognizer.resolveTime([results.response]);
+        session.beginDialog('askForPartySize');
+    },
+    function (session, results) {
+        session.dialogData.partySize = results.response;
+        session.beginDialog('askForReserverName');
+    },
+    function (session, results) {
         session.dialogData.reservationName = results.response;
-        builder.Prompts.choice(session, "Table cloth color preferance?", "red|green|blue", { listStyle: 3 });
+        session.beginDialog('askForTableCloth');
     },
     function (session, results) {
         session.dialogData.clothColor =  results.response.entity;
@@ -29,8 +117,11 @@ var bot = new builder.UniversalBot(connector, [
         session.send(`Reservation confirmed. Reservation details: <br/>Date/Time: ${session.dialogData.reservationDate} <br/>Party size: ${session.dialogData.partySize} <br/>Reservation name: ${session.dialogData.reservationName} <br/>Table cloth color: ${session.dialogData.clothColor}`);
         session.endDialog();
     }
-]);
-
+])
+.triggerAction({
+    matches: /^dinner reservation$/i,
+    confirmPrompt: "This will cancel your current request. Are you sure?"
+});
 // Enable Conversation Data persistence
 bot.set('persistConversationData', true);
 
