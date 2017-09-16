fda9ab177b6101002e99a8b9873203d52de715d8
Abhishek Simla <live.abhishek@outlook.com>
2017-09-16 22:11:35 +0530

    Added log4js

---
+++ bot/log4js.js
@@ -0,0 +1,13 @@
+const log4js = require('log4js');
+log4js.configure({
+  appenders: { 
+      fileappender: { type: 'file', filename: 'probot.log' },
+      consoleappender: {type: 'console'}
+    },
+  categories: { default: { appenders: ['fileappender', 'consoleappender'], level: 'debug' } }
+});
+
+const logger = log4js.getLogger('probot');
+module.exports = {
+    logger: logger
+}
