0b25ed7e2f108c4605feeeceab39ef232e5c5860
Abhishek Simla <live.abhishek@outlook.com>
2017-09-20 00:14:22 +0530

    Updated show holidays to support date ranges Added new rest endpoints

--- bot/bot/prorigoRest.js
+++ bot/bot/prorigoRest.js
@@ -3,16 +3,39 @@ require('isomorphic-fetch');
 
 
 var prorigoRestEndpoint = process.env.PRORIGO_REST_SERVER;
-function getAllHolidays(){
-    return fetch(prorigoRestEndpoint + '/api/holidays/all')
-	.then(function(response) {
-		if (response.status >= 400) {
-			throw new Error("Bad response from server");
-		}
-		return response.json();
-	});
+function getAllHolidays(callback, errCallback){
+    fetch(prorigoRestEndpoint + '/api/holidays/all')
+    .then(function(response) {
+        if (response.status >= 400) {
+            throw new Error("Bad response from server");
+        }
+        return response.json();
+    }).then(function(jsonResponse){
+        callback(jsonResponse);
+    }).catch(function(err){
+        errCallback(err);
+    });;
+}
+
+function getHolidays(callback, errCallback, body){
+    var headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    };
+    fetch(prorigoRestEndpoint + '/api/holidays', {method: 'POST', body: JSON.stringify(body), headers: headers})
+    .then(function(response){
+        if (response.status >= 400) {
+            throw new Error("Bad response from server");
+        }
+        return response.json();
+    }).then(function(jsonResponse){
+        callback(jsonResponse);
+    }).catch(function(err){
+        errCallback(err);
+    })
 }
 
 module.exports = {
-    getAllHolidays: getAllHolidays
+    getAllHolidays: getAllHolidays,
+    getHolidays: getHolidays
 }
