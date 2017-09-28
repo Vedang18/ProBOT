5f940f381991c28683787a446dfa84d29dd8be8a
Abhishek Simla <live.abhishek@outlook.com>
2017-09-28 10:57:45 +0530

    Added date file appender in logs Added some entries in gitignore

--- bot/log4js.js
+++ bot/log4js.js
@@ -1,7 +1,7 @@
 var log4js = require('log4js');
 log4js.configure({
   appenders: { 
-      fileappender: { type: 'file', filename: 'probot.log' },
+      fileappender: { type: 'dateFile', filename: 'probot.log', pattern: "-yyyy-MM-dd", alwaysIncludePattern: true},
       consoleappender: {type: 'console'}
     },
   categories: { default: { appenders: ['fileappender', 'consoleappender'], level: 'debug' } }
